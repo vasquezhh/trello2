294ba1705d4fc8ba1e5aada2678f44aaf4c2cce7
Hainer Vasquez <vasquezh@uninorte.edu.co>
2015-11-10 02:27:23 +0000

    Nombres y validacion

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_filter :get_users
-  
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   # GET /tasks
   # GET /tasks.json
